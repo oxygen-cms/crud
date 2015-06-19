b438ec691474eefaf064d923fc9ea8453774a7e1
Chris Chamberlain <c.chamberlain@iinet.net.au>
2015-06-19 18:44:58 +1000

    Updated form namespace

--- resources/views/basic/createForm.blade.php
+++ resources/views/basic/createForm.blade.php
@@ -1,6 +1,6 @@
 <?php
 
-use Oxygen\Core\Form\Form;use Oxygen\Core\Html\Form\EditableField;
+use Oxygen\Core\Html\Form\Form;use Oxygen\Core\Html\Form\EditableField;
     use Oxygen\Core\Html\Form\Footer;use Oxygen\Core\Html\Form\Label;use Oxygen\Core\Html\Form\Row;use Oxygen\Core\Html\Toolbar\ButtonToolbarItem;use Oxygen\Core\Html\Toolbar\SubmitToolbarItem;
 
 ?>
